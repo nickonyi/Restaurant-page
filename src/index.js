22ba10589898ac7eaf32692df0f2a1e46fc36b6f
nickonyi <nickoluoch1130@gmail.com>
2023-04-22 18:14:44 +0300

    project setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { pageLoard } from "./page-load";
 import './style.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+// use the faHome icon in your code
+console.log(faHome);
 
 
 console.log("New project baby");
--- src/page-load.js
+++ src/page-load.js
@@ -1,5 +1,6 @@
 import img from './rest-1.jpg';
 import imeg from './don.png';
+import emo from './oval.png';
 
 function pageLoard() {
     const content = document.getElementById('content');
@@ -16,9 +17,11 @@ function pageLoard() {
     image.classList.add('img');
     image.src = imeg;
     para1.textContent = " Variety of menu: A good restaurant should offer a diverse menu, with something for everyone, including vegetarians or people with allergies.</li>   Friendly, attentive staff: The staff should be friendly, attentive, and knowledgeable about the menu. They should also make you feel welcome and comfortable.Cleanliness: The restaurant should be clean and well-maintained, with tidy restrooms and comfortable seating arrangements.";
+    const ovali = new Image();
+    ovali.src = emo;
     para2.textContent = "Price: The best restaurant should offer good value for money, with reasonable and affordable prices.Ambience: The atmosphere of a restaurant can greatly affect the dining experience, so the best restaurant should have an atmosphere that suits your preferences, whether it be formal or casual, romantic or familial, lively or tranquil.always helpful to read reviews from other customers to get a sense of what others think of the restaurant. You can check out review sites like Yelp, TripAdvisor, or Google Reviews, to learn more about a particular restaurant"
 
-    const elements = [headingO, myImage, image, para, para1, para2];
+    const elements = [headingO, myImage, image, ovali, para, para1, para2];
     for (let i = 0; i < elements.length; i++) {
         content.appendChild(elements[i]);
 
